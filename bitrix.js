5c5af5cb45d4877ecd2621f40f801b45dcb197de
FearlessDevf1 <fearlessdevf1@gmail.com>
2024-11-30 00:30:34 +0100

    Add files via upload

---
+++ bitrix.js
@@ -0,0 +1,15 @@
+function show_pop(){
+y=document.getElementById("po_updiv").style.display='block';
+}
+
+function close_pop(){
+document.getElementById("po_updiv").style.display='none';
+}
+
+function show_pop2(){
+y=document.getElementById("po_updiv2").style.display='block';
+}
+
+function close_pop2(){
+document.getElementById("po_updiv2").style.display='none'
+}
